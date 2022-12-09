b4c980cb8674251ce336940527e75c901bca8eab
Levente Botos <66298417+LeventeBotos@users.noreply.github.com>
2022-12-09 08:25:02 +0100

    Create game.py

---
+++ game.py
@@ -0,0 +1,12 @@
+kukacok = input("Hány kukac?\n")
+kuk = int(kukacok)
+print(kukacok)
+for i in range(kuk):
+   player1 = True
+   n = input(("Hanyat akarsz levenni?\n"))
+   ni = int(n)
+   val = kuk - ni
+
+print("done")
+if(player1 != True):
+   print("1 nyert")
